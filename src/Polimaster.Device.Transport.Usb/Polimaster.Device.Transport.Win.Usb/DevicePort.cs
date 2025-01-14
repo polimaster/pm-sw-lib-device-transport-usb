a074cf675959bfeab1d2dd26fc5c0247afb45ff1
Alexander Voronische <voronizer@gmail.com>
2025-01-14 15:37:06 +0200

    update DevicePort remove async callbacks

--- src/Polimaster.Device.Transport.Usb/Polimaster.Device.Transport.Win.Usb/DevicePort.cs
+++ src/Polimaster.Device.Transport.Usb/Polimaster.Device.Transport.Win.Usb/DevicePort.cs
@@ -1,58 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Threading;
+﻿using System.IO.Ports;
 
 namespace Polimaster.Device.Transport.Win.Usb;
 
 /// <inheritdoc cref="IDevicePort" />
 public class DevicePort : SerialPort, IDevicePort {
 
-    private ManualResetEvent? resetEvent;
-    private List<byte> readBuffer = [];
-    private SerialError? readError;
-
     /// <inheritdoc />
     public DevicePort(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits) :
         base(portName, baudRate, parity, dataBits, stopBits) {
+        // RtsEnable = true;
     }
 
     /// <inheritdoc />
-    public void Write(byte[] buffer) => Write(buffer, 0, buffer.Length);
+    public void Write(byte[] buffer) {
+        WriteLine(System.Text.Encoding.UTF8.GetString(buffer));
+        // Write(buffer, 0, buffer.Length);
+    }
 
     /// <inheritdoc />
     public byte[] Read() {
-        DataReceived += OnDataReceived;
-        ErrorReceived += OnErrorReceived;
-        readBuffer = [];
-        readError = null;
-        resetEvent = new ManualResetEvent(false);
-        resetEvent.WaitOne();
-        resetEvent.Reset();
-
+        var str = ReadLine();
+        return System.Text.Encoding.UTF8.GetBytes(str);
         // var res = new List<byte>();
         // while (BytesToRead > 0) {
         //     var buffer = new byte[BytesToRead];
         //     Read(buffer, 0, buffer.Length);
         //     res.AddRange(buffer);
         // }
-
-        DataReceived -= OnDataReceived;
-        ErrorReceived -= OnErrorReceived;
-        if (readError != null) throw new Exception($"Data read error: {readError}");
-        return readBuffer.ToArray();
-    }
-
-    private void OnErrorReceived(object sender, SerialErrorReceivedEventArgs e) {
-        readError = e.EventType;
-        resetEvent?.Set();
-    }
-
-    private void OnDataReceived(object sender, SerialDataReceivedEventArgs e) {
-        if (e.EventType == SerialData.Eof) resetEvent?.Set();
-
-        var buffer = new byte[BytesToRead];
-        Read(buffer, 0, BytesToRead);
-        readBuffer.AddRange(buffer);
+        // return res.ToArray();
     }
 }
